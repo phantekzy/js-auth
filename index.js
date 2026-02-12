9e4eda9c2d89dccfcb530f77dc10637f784fd35a
phantekzy <mainilotfi@gmail.com>
2026-02-12 17:56:50 +0100

    changed the structure

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import "dotenv/config";
+import bcrypt from "bcrypt";
 import express from "express";
 
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("The Auth Server is Running");
+  res.send("The Auth server is Running");
 });
+app.post("/register", async (req, res) => {});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is Alive on https://localhost:${PORT}`);
+  console.log(`Server is Alive on https:://localhost:${PORT}`);
 });
