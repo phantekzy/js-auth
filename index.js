e0482a6f22e8077ddfe9323fb2eed35fd5f0928a
phantekzy <mainilotfi@gmail.com>
2026-02-12 17:42:53 +0100

    i added the port so we can listen to it

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ app.get("/", (req, res) => {
   res.send("The Auth Server is Running");
 });
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is Alive on https://localhost:${PORT}`);
+});
