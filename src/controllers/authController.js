5e72399ab5d11ebe3a2af73a5a5a26a978efccda
phantekzy <mainilotfi@gmail.com>
2026-02-13 20:45:12 +0100

    writing the authControler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,1 +1,15 @@
-// Auth controler
+import pool from "../config/db.js";
+import bcrypt from "bcrypt";
+
+export const registerUsers = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Both email and password are required" });
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+  } catch (error) {}
+};
