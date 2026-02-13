efec6c70112fc3bc211f5505c74953ae0a0f1c11
phantekzy <mainilotfi@gmail.com>
2026-02-13 23:39:02 +0100

    i finished the login logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -28,3 +28,35 @@ export const registerUser = async (req, res) => {
   console.error("Reg Error:", error);
   return req.status(500).json({ error: "Server Error during registration" });
 };
+
+// Login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Both email and password are required!" });
+    }
+    // search the user
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1 RETURNING id,email",
+      [email],
+    );
+    if (result.rows === 0) {
+      return res.status(401).json({ message: "invalid mail or password" });
+    }
+    const user = result.rows[0];
+    // Comparing the passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "invalid mail or password" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Login successfull", user: result.rows[0] });
+  } catch (error) {
+    console.error("Login Error", error.message);
+    return res.status(500).json({ error: "Server Error during the loading" });
+  }
+};
