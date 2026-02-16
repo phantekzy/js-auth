448dffdb9e1cb47ba3c7f8df24353a732d3eb904
phantekzy <mainilotfi@gmail.com>
2026-02-16 20:53:15 +0100

    checking errors

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,17 +1,20 @@
 import jwt from "jsonwebtoken";
+
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
+
   if (!token) {
     return res
-      .stauts(403)
+      .status(403)
       .json({ message: "Access denied: No token provided" });
   }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    return res.stauts(401).json({ message: "Invalid or expired token" });
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
