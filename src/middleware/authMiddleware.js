cf1bab50d974322c6a33676f1a22b9c74780bcb3
phantekzy <mainilotfi@gmail.com>
2026-02-16 20:45:07 +0100

    finished the middleware and implemented jwt

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
@@ -5,5 +6,12 @@ export const verifyToken = (req, res, next) => {
     return res
       .stauts(403)
       .json({ message: "Access denied: No token provided" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.stauts(401).json({ message: "Invalid or expired token" });
   }
 };
