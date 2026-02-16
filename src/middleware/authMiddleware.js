5475ea58d5ef33415da4d9443874ec9239f3c12d
phantekzy <mainilotfi@gmail.com>
2026-02-16 20:41:23 +0100

    fml i forgoet how to write an arrow function

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,9 @@
-export const verifyToken = (req,res,next){
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    
-}
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res
+      .stauts(403)
+      .json({ message: "Access denied: No token provided" });
+  }
+};
