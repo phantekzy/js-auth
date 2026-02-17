1b2c06c0c1c85fcdedfa41208e73ef79bf6794a7
phantekzy <mainilotfi@gmail.com>
2026-02-17 02:45:51 +0100

    still on the router

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,3 +1,6 @@
 import express from "express";
+import { loginUser, registerUser } from "../controllers/authController";
 
 const router = express.Router();
+router.post("/register", registerUser);
+router.post("/login", loginUser);
