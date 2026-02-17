a83fdf9a47fa7f72c7b8af2be450de3ca7e48e8c
phantekzy <mainilotfi@gmail.com>
2026-02-17 02:49:49 +0100

    added a Private route tests if JWT is working

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,15 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/authController";
+import { registerUser, loginUser } from "../controllers/authController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Private route: tests if JWT is working
+router.get("/profile", verifyToken, (req, res) => {
+  res.status(200).json({ message: "Access granted", user: req.user });
+});
+
+export default router;
