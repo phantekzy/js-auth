71922f02186cc6102dea5d01e5da7dfffc90d15c
phantekzy <mainilotfi@gmail.com>
2026-02-17 02:40:42 +0100

    auth controler completed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,31 +1,28 @@
-import pool from "../config/db";
+import pool from "../config/db.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-// Register users
-export const regusterUser = async (req, res) => {
+export const registerUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Both Email and Password are required!" });
+      return res.status(400).json({ message: "Email and password required" });
     }
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
     const result = await pool.query(
-      "INSERT INTO users(email,password) VALUES ($1,$2) RETURNING id,email",
+      "INSERT INTO users(email, password) VALUES ($1, $2) RETURNING id, email",
       [email, hashedPassword],
     );
-    res.status(200).json({ message: "User created", user: result.rows[0] });
+
+    res.status(201).json({ message: "User created", user: result.rows[0] });
   } catch (error) {
     if (error.code === "23505")
       return res.status(400).json({ message: "Email already exists" });
-    res.status(500).json({ error: "Server Error during registration" });
+    res.status(500).json({ error: "Server error during registration" });
   }
 };
 
-// Login users
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -34,15 +31,24 @@ export const loginUser = async (req, res) => {
     ]);
     if (result.rows.length === 0)
       return res.status(401).json({ message: "Invalid credentials" });
+
     const user = result.rows[0];
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
+    if (!isMatch)
       return res.status(401).json({ message: "Invalid credentials" });
-    }
+
     const token = jwt.sign(
       { id: user.id, email: user.email },
       process.env.JWT_SECRET,
       { expiresIn: "1h" },
     );
-  } catch (error) {}
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: { id: user.id, email: user.email },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error during login" });
+  }
 };
