e3afbd12da79381ae758a5445f5733e6e77917c4
phantekzy <mainilotfi@gmail.com>
2026-02-19 00:10:14 +0100

    creating AuthContext and Provider

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,3 +1,6 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext<any>(null);
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<any>(null);
+}
