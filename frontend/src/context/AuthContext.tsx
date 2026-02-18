9f9cb3569db780b29f2e7f32c7e80111b585686c
phantekzy <mainilotfi@gmail.com>
2026-02-19 00:12:29 +0100

    checking Authentification

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,15 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext<any>(null);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<any>(null);
+    const [loading, setLoadin] = useState(true);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const token = localStorage.getItem("token");
+        }
+    }, [])
+
+
 }
