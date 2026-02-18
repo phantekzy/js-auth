4f4e441f2b8e29cba465a99ac3583978fd027392
phantekzy <mainilotfi@gmail.com>
2026-02-19 00:40:59 +0100

    authProvider

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -4,22 +4,20 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext<any>(null);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<any>(null);
-    const [loading, setLoadin] = useState(true);
-
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const checkAuth = async () => {
             const token = localStorage.getItem("token");
             if (token) {
                 try {
-                    const res = await axios.get("http://localhost:6969//api/auth/profile",
-                        { headers: { Authorization: `Bearer ${token}` } }
-                    )
+                    const res = await axios.get('http://localhost:6969//api/auth/profile', {
+                        headers: { Authorization: `Bearer ${token}` }
+                    })
+                    setUser(res.data.user)
                 } catch (error) {
 
                 }
             }
         }
     }, [])
-
-
 }
