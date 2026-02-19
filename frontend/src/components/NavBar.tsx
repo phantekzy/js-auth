fe7f95d94ce7be36626f880d714dd0eba35099c4
phantekzy <mainilotfi@gmail.com>
2026-02-19 17:07:01 +0100

    im not satisfied

--- frontend/src/components/NavBar.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-
-export default function Navbar() {
-    const { user, logout } = useContext(AuthContext)
-}
