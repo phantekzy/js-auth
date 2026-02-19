6aae37314a87dd6a61b5beeeb4cf131e3ce6e6dc
phantekzy <mainilotfi@gmail.com>
2026-02-19 15:48:50 +0100

    finished the input component

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,7 +1,11 @@
 export default function InputField({ label, ...props }: any) {
     return (
         <div className="mb-4 text-left">
-
+            <label className="block text-sm font-semibold text-gray-700 mb-1">{label}</label>
+            <input
+                {...props}
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all"
+            />
         </div>
-    )
+    );
 }
