4e6fd9525acbeb7d7c4f14d3d745947c651eb6a2
phantekzy <mainilotfi@gmail.com>
2026-02-19 01:01:36 +0100

    well as usual late night coding full of stupid typos

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,35 +1,38 @@
-import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const AuthContext = createContext<any>(null);
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<any>(null);
     const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const checkAuth = async () => {
-            const token = localStorage.getItem("token");
+            const token = localStorage.getItem('token');
             if (token) {
                 try {
                     const res = await axios.get('http://localhost:6969/api/auth/profile', {
                         headers: { Authorization: `Bearer ${token}` }
-                    })
-                    setUser(res.data.user)
+                    });
+                    setUser(res.data.user);
                 } catch (err) {
-                    console.error(err);
-                    localStorage.removeItem("token")
+                    localStorage.removeItem('token');
                 }
             }
             setLoading(false);
-        }
+        };
         checkAuth();
-    }, [])
+    }, []);
+
     const logout = () => {
-        localStorage.removeItem("token");
+        localStorage.removeItem('token');
         setUser(null);
-    }
+    };
+
     return (
-        <AuthContext.Provider value={{ user, setUser, lodaing, setLoading }}>
+        <AuthContext.Provider value={{ user, setUser, logout, loading }}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
