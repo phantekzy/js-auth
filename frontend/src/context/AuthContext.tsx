299d3801df87ca7ab2c8468e7fc275e85ec96661
phantekzy <mainilotfi@gmail.com>
2026-02-20 17:24:20 +0100

    check auth

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,3 +1,5 @@
+import { createContext, useState, type ReactNode } from "react";
+
 interface User {
     id: string;
     email: string;
@@ -8,4 +10,10 @@ interface AuthContextType {
     setUser: React.Dispatch<React.SetStateAction<User | null>>;
     loading: boolean;
     logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null)
+    const [loading, setLoading] = useState(true);
 }
