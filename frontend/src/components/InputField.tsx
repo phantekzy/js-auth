b21486c980cf1b3750ff1885ff3407bffb97c5ab
phantekzy <mainilotfi@gmail.com>
2026-02-20 17:56:40 +0100

    Reusable Input with Types

---
+++ frontend/src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import type { InputHTMLAttributes } from "react"
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    lable: string;
+}
+
+
+const InputField = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default InputField
