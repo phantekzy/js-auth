c4d45256835e1677f1898da23aaca563bee966c6
phantekzy <mainilotfi@gmail.com>
2026-02-21 15:15:42 +0100

    fixed the architeture of the project

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,20 +1,19 @@
-import type { InputHTMLAttributes } from "react";
+import { type InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
 }
 
-const InputField = ({ label, ...props }: InputProps) => {
+export default function InputField({ label, ...props }: InputProps) {
     return (
         <div className="mb-4 text-left">
-            <label className="block text-sm font-semibold text-gray-700 mb-1">{label}</label>
+            <label className="block text-sm font-semibold text-gray-700 mb-1">
+                {label}
+            </label>
             <input
                 {...props}
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2
-                focus:ring-blue-500 focus:outline-none transition-all"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all"
             />
         </div>
-    )
+    );
 }
-
-export default InputField
