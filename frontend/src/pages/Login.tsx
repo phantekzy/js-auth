4b9c1f8ca09ac5eabe7517f0eb2455ce52fb23bd
phantekzy <mainilotfi@gmail.com>
2026-02-21 15:36:27 +0100

    Ich implementiere gerade die handleSubmit-Funktion

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,24 @@
-import { useState } from "react"
+import { useState, type FormEvent } from "react"
 import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const { setUser } = useAuth();
+    const navigate = useNavigate();
 
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        try {
+            const res = await api.post('/auth/login', { email, password })
+            localStorage.setItem('token', res.data.token);
+            setUser(res.data.user);
+            navigate('/');
+        } catch (error) {
+
+        }
+    }
 
 
 
