aa663e7f711344ec94d7782f3c1b425f52c02c78
phantekzy <mainilotfi@gmail.com>
2026-02-21 15:08:53 +0100

    added a reusable component for inputs

--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -1,13 +1,19 @@
-import type { InputHTMLAttributes } from "react"
+import type { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    lable: string;
+    label: string;
 }
 
-
-const InputField = () => {
+const InputField = ({ label, ...props }: InputProps) => {
     return (
-        <div></div>
+        <div className="mb-4 text-left">
+            <label className="block text-sm font-semibold text-gray-700 mb-1">{label}</label>
+            <input
+                {...props}
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2
+                focus:ring-blue-500 focus:outline-none transition-all"
+            />
+        </div>
     )
 }
 
