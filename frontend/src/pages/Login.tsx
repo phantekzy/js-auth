de8ed3d785563ea337bdb7cea257d28adb66f677
phantekzy <mainilotfi@gmail.com>
2026-02-24 19:14:56 +0100

    gotta rewrite the login in type safe i forgot im on typescript

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,51 +1,7 @@
-import { useNavigate } from 'react-router-dom';
-import api from '../api/axios';
-import { useAuth } from '../hooks/useAuth';
-import InputField from '../components/InputField';
-import { useState, type FormEvent } from 'react';
-
-export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { setUser } = useAuth();
-    const navigate = useNavigate();
-
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        try {
-            const res = await api.post('/auth/login', { email, password });
-            localStorage.setItem('token', res.data.token);
-            setUser(res.data.user);
-            navigate('/');
-        } catch (err: any) {
-            alert(err.response?.data?.message || "Login failed");
-        }
-    };
-
+const Login = () => {
     return (
-        <div className="flex items-center justify-center pt-10">
-            <div className="w-full max-w-md p-8 bg-white rounded-3xl shadow-xl border border-gray-100">
-                <h2 className="text-3xl font-bold mb-6 text-gray-900">Sign In</h2>
-                <form onSubmit={handleSubmit}>
-                    <InputField
-                        label="Email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                    <InputField
-                        label="Password"
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                    <button className="w-full bg-blue-600 text-white py-3 rounded-xl font-bold hover:bg-blue-700 transition mt-4">
-                        Login
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
+        <div></div>
+    )
 }
+
+export default Login
