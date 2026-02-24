d97871042ed6861fbafafcd16c32ba4c59da4f30
phantekzy <mainilotfi@gmail.com>
2026-02-24 18:45:58 +0100

    on the login section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
 
 const App = () => {
     return (
-        <div>
-            <Login />
-        </div>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+        </Routes>
+
     )
 }
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,20 +8,24 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const { setUser } = useAuth();
     const navigate = useNavigate();
-
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         try {
             const res = await api.post('/auth/login', { email, password })
-            localStorage.setItem('token', res.data.token)
-            setUser(res.data.user)
-            navigate('/')
+            localStorage.setItem('token', res.data.token);
+            setUser(res.data.user);
+            navigate('/');
         } catch (err: any) {
-            alert(err.response?.data?.message || 'Login failed');
+            alert(err.response?.data?.message || "Login Failed")
         }
     }
     return (
-        <div></div>
+        <div className="flex items-center justify-center pt-10">
+            <div className="w-full max-w-md p-8 bg-white rounded-3xl shadow-xl border border-gray-100">
+                <h1>Login</h1>
+            </div>
+
+        </div>
     )
 }
 
