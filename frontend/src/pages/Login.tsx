f4e97a84076d194ae79d14125e79bf4410412eab
phantekzy <mainilotfi@gmail.com>
2026-02-24 19:17:08 +0100

    typing states

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 const Login = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
     return (
         <div></div>
     )
